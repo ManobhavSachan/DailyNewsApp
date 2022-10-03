231a47ffeead8ed38073b28436e2e24f712b1053
ManobhavSachan <94208034+ManobhavSachan@users.noreply.github.com>
2022-10-03 13:39:23 +0530

    Add files via upload

---
+++ build/precache-manifest.5d2299fd54a033c67f93029b8ebd9e06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a1336e1618a5372b3bc",
+    "url": "/static/css/main.9b64f201.chunk.css"
+  },
+  {
+    "revision": "8a1336e1618a5372b3bc",
+    "url": "/static/js/main.8a1336e1.chunk.js"
+  },
+  {
+    "revision": "b4580c6e423a71b044a8",
+    "url": "/static/js/1.b4580c6e.chunk.js"
+  },
+  {
+    "revision": "813df4848237cb172fa9",
+    "url": "/static/css/2.65f024b8.chunk.css"
+  },
+  {
+    "revision": "813df4848237cb172fa9",
+    "url": "/static/js/2.813df484.chunk.js"
+  },
+  {
+    "revision": "9ebd49d8dd78d63c29fc",
+    "url": "/static/js/runtime~main.9ebd49d8.js"
+  },
+  {
+    "revision": "6c575bf555021e57f05b3b92f0d1900a",
+    "url": "/static/media/loading.6c575bf5.gif"
+  },
+  {
+    "revision": "1a667cf6ccad430b48a0d73f4f777e54",
+    "url": "/index.html"
+  }
+];
